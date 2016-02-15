3a2e0902211e25e681e7a3eec64138aa827358e3
Dennis Durling <djdtahoe@gmail.com>
2016-02-15 13:29:45 -0800

    forgot to add new download link

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.6.2',
     packages=['flask_datatables', 'flask_datatables.views'],
     url='https://github.com/tahoe/flask_datatables/',
-    download_url='https://github.com/tahoe/flask_datatables/tarball/0.6.0',
+    download_url='https://github.com/tahoe/flask_datatables/tarball/0.6.2',
     license='MIT',
     long_description=desc,
     keywords='sqlalchemy datatables jquery flask-restless flask-restful',
