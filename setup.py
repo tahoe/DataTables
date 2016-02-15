9c5e9f0bda55f95f449f1c7cbea52fbafb0b2c20
Dennis Durling <djdtahoe@gmail.com>
2016-02-15 13:06:33 -0800

    forgot to add sub packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ desc = open("readme.rst").read() if os.path.isfile("readme.rst") else ""
 setup(
     name='flask_datatables',
     version='0.6.0',
-    packages=['flask_datatables'],
+    packages=['flask_datatables', 'flask_datatables.views],
     url='https://github.com/tahoe/flask_datatables/',
     download_url='https://github.com/tahoe/flask_datatables/tarball/0.6.0',
     license='MIT',
