d0156fc5b1f1dc1d5f9242fa106212f3a29b2b19
Dennis Durling <djdtahoe@gmail.com>
2016-02-15 13:30:54 -0800

    updated version so download link patches

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ desc = open("readme.rst").read() if os.path.isfile("readme.rst") else ""
 
 setup(
     name='flask_datatables',
-    version='0.6.2',
+    version='0.6.3',
     packages=['flask_datatables', 'flask_datatables.views'],
     url='https://github.com/tahoe/flask_datatables/',
-    download_url='https://github.com/tahoe/flask_datatables/tarball/0.6.2',
+    download_url='https://github.com/tahoe/flask_datatables/tarball/0.6.3',
     license='MIT',
     long_description=desc,
     keywords='sqlalchemy datatables jquery flask-restless flask-restful',
